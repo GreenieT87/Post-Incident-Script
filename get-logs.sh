a8322427048274501eb21894a012169e5c83683a
GreenieT87 <35691764+GreenieT87@users.noreply.github.com>
2018-11-28 13:19:01 +0100

    Initial commit

---
+++ get-logs.sh
@@ -0,0 +1,47 @@
+﻿#!/usr/bin/env bash
+echo " "
+echo "You will need Root permissions to run this properly"
+echo "checking if $HOME/post_incident_logs exits, if so empty it, if not create it",
+echo " "
+[ ! -d $HOME/post_incident_logs/ ] && mkdir -p $HOME/post_incident_logs
+[ -d $HOME/post_incident_logs/ ] && rm -r $HOME/post_incident_logs/*
+DIRECTORY="$HOME/post_incident_logs/"
+TIME=$(date)
+HOSTNAME=$(hostname)
+
+echo " "
+echo " "
+
+echo "Please check FileSizes : "
+#find /var/log/ -type f \( -name "*.log" -or -name "*.out" -or -name "*.txt" \) -exec ls -lha {} \; |awk '{ print $5 " "$9 }'|sort -h
+TOTAL=$(find /var/log/ -type f \( -name "*.log" -or -name "*.out" -or -name "*.txt" \) -exec ls -la {} \; |awk '{ sum=sum+$5 } END { print (sum/1024)/1024 " MB" } ')
+echo " "
+echo "Total size of logs: $TOTAL"
+echo " "
+echo "Copy the logs to $DIRECTORY. Sure to proceed? [y/n]:"
+read key
+if [ $key = "y" ];
+then
+  find /var/log/ -type f \( -name "*.log" -or -name "*.out" -or -name "*.txt" \) -exec cp --parents {} "$DIRECTORY" \;
+else
+  echo "EXIT"
+  exit 1
+fi
+echo "Want to Zip the logs and sent them via Mail? [y/n]"
+read key
+if [ $key = "y" ];
+then
+  zip -r logs.zip $DIRECTORY >/dev/null
+else
+  echo "EXIT"
+  exit 1
+fi
+#echo "Want a Mail with the zip file? [y/n]"
+#read key
+#if [ $key = "y" ];
+#then
+  swaks --to t.gruen@tafmobile.de --attach logs.zip --header "Subject: Logs zur Störung auf $HOSTNAME am $TIME" --body " " >/dev/null
+#else
+#  echo "EXIT"
+#  exit 1
+#fi
